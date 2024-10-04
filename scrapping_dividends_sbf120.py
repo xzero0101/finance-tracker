4769d03551bcf1b5cbfc017433d55a300f4d9de5
fabien bidault <fabienbidault@outlook.fr>
2024-10-05 01:46:36 +0200

    Scraping 2024 dividends from a website

--- scrapping_dividends_sbf120.py
+++ scrapping_dividends_sbf120.py
@@ -1,15 +1,13 @@
-# scrapping dividends from sbf120 in boursorama
-# url = https://www.boursorama.com/bourse/actions/palmares/dividendes/page- + (number of page)
-# get data from rows
-# display data in console
-
-import requests, json
+import requests, json, os
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+load_dotenv()
 
 sbf_120 = {}
 
 for i in range(1, 10):  
-    url = "https://www.boursorama.com/bourse/actions/palmares/dividendes/page-" + str(i)
+    url = os.getenv('URL') + str(i)
     response = requests.get(url)    
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -18,9 +16,9 @@ for i in range(1, 10):
         for row in rows:
             cols = row.find_all('td')
             if len(cols) > 5:
-                name_company = cols[0].text.strip().replace("SRD", "").replace(" ", "").replace("\n", "")
+                company = cols[0].text.strip().replace("SRD", "").replace(" ", "").replace("\n", "")
                 dividend_2024 = cols[5].text.strip()
-                sbf_120[name_company] = dividend_2024
+                sbf_120[company] = dividend_2024
 
 sorted_dict = {k: v for k, v in sorted(sbf_120.items(), key=lambda item: item[1], reverse=True)}
 print(json.dumps(sorted_dict, indent=4))
